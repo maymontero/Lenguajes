3d5caa5656fa1c312a7469fbbed9149b30727489
Francisco Piria <franpiria42@gmail.com>
2020-08-28 22:09:38 -0300

    ejercicio 1.3 prolog

--- Prolog/lengprog.pl
+++ Prolog/lengprog.pl
@@ -22,3 +22,13 @@ inBetween(A, B, X) :-
 inBetween(A, X, B) :-
     X =< B,
     X >= A.
+
+factorial(N, F) :-
+    N > 0,
+    N1 is N-1,
+    factorial(N1,F1),
+    F is N * F1.
+
+factorial(N, F) :-
+    N = 0,
+    F = 1.
