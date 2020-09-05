a1b13de30825b993c3c866549f8ffb8b9285f410
Francisco Piria <franpiria42@gmail.com>
2020-09-04 22:37:17 -0300

    ejercicios 4/9

--- Prolog/lengprog.pl
+++ Prolog/lengprog.pl
@@ -36,3 +36,99 @@ zip([_], [], []).
 zip([], [_], []).
 zip([X|Xs], [Y|Ys], [pair(X,Y)|XYs]) :-
    zip(Xs, Ys, XYs).
+
+treeHeight(nil, 0).
+treeHeight(tree(V, L, R), H) :-
+    treeHeight(L, LH),
+    treeHeight(R, RH),
+    H is (max(LH, RH) + 1).
+
+begin(tictactoe(
+    empty, empty, empty,
+    empty, empty, empty,
+    empty, empty, empty)).
+
+winner(tictactoe(
+    P, P, P,
+    _, _, _,
+    _, _, _), P) :-
+    P \= empty.
+winner(tictactoe(
+    _, _, _,
+    P, P, P,
+    _, _, _), P) :-
+    P \= empty.
+winner(tictactoe(
+    _, _, _,
+    _, _, _,
+    P, P, P), P) :-
+    P \= empty.
+winner(tictactoe(
+    P, _, _,
+    P, _, _,
+    P, _, _), P) :-
+    P \= empty.
+winner(tictactoe(
+    _, P, _,
+    _, P, _,
+    _, P, _), P) :-
+    P \= empty.
+winner(tictactoe(
+    _, _, P,
+    _, _, P,
+    _, _, P), P) :-
+    P \= empty.
+winner(tictactoe(
+    P, _, _,
+    _, P, _,
+    _, _, P), P) :-
+    P \= empty.
+winner(tictactoe(
+    _, _, P,
+    _, P, _,
+    P, _, _), P) :-
+    P \= empty.
+
+loser(T, playerX) :-
+    winner(T, playerO).
+loser(T, playerO) :-
+    winner(T, playerX).
+
+tictactoe2List(tictactoe(S0, S1, S2, S3, S4, S5, S6, S7, S8), 
+    [S0, S1, S2, S3, S4, S5, S6, S7, S8]).
+
+emptyCount(T, N) :-
+    tictactoe2List(T, TL),
+    auxEmptyCount(TL, N).
+
+auxEmptyCount([], 0).
+auxEmptyCount([empty|SS], N) :-
+    auxEmptyCount(SS, M),
+    N is M+1.
+auxEmptyCount([S|SS], N) :-
+    S \= empty,
+    auxEmptyCount(SS, N).
+
+activePlayer(T, P) :-
+    emptyCount(T, N),
+    A is mod(N, 2),
+    decidePlayer(A, P).
+
+decidePlayer(1, playerX).
+decidePlayer(0, playerO).
+
+emptySquare(T, N) :-
+    tictactoe2List(T, TL),
+    nth0(N, TL, empty).
+
+validAction(T, mark(P, I)) :-
+    activePlayer(T, P),
+    emptySquare(T, I).
+
+actions(T, AS) :-
+    activePlayer(T, P),
+    include(validAction(T), [
+        mark(P, 0), mark(P, 1), mark(P, 2),
+        mark(P, 3), mark(P, 4), mark(P, 5),
+        mark(P, 6), mark(P, 7), mark(P, 8)
+    ], AS).
