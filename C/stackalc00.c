012b6e51926abbb5efec9cd132ffe80656c7d414
May <maymmiqueiro@gmail.com>
2020-10-02 22:31:42 -0300

    Ejercicio 02/10
    Por algun motivo no imprime el stack (tiene problema con el *top)

--- C/stackalc00.c
+++ C/stackalc00.c
@@ -24,12 +24,149 @@ void evaluate(char **code, int *pos, int *stack, int *top) {
             int newTop = *top-1;
             *top = newTop;
         }
-    } else {
+    } else if(strcmp("EQ", instruction) == 0) {
+        if(n1 == n2)
+            stack[*top-1] = 1;
+        else
+            stack[*top-1] = 0;
+        
+        int newTop = *top-1;
+        *top = newTop;
+    } else if(strcmp("DIFF", instruction) == 0) {
+        if(n1 == n2)
+            stack[*top-1] = 0;
+        else
+            stack[*top-1] = 1;
+        
+        int newTop = *top-1;
+        *top = newTop;
+    } else if(strcmp("LT", instruction) == 0) {
+        if(n2 < n1)
+            stack[*top-1] = 1;
+        else
+            stack[*top-1] = 0;
+        
+        int newTop = *top-1;
+        *top = newTop;
+    } else if(strcmp("LTE", instruction) == 0) {
+        if(n2 <= n1)
+            stack[*top-1] = 1;
+        else
+            stack[*top-1] = 0;
+        
+        int newTop = *top-1;
+        *top = newTop;
+    } else if(strcmp("GT", instruction) == 0) {
+        if(n2 > n1)
+            stack[*top-1] = 1;
+        else
+            stack[*top-1] = 0;
+        
+        int newTop = *top-1;
+        *top = newTop;
+    } else if(strcmp("GTE", instruction) == 0) {
+        if(n2 >= n1)
+            stack[*top-1] = 1;
+        else
+            stack[*top-1] = 0;
+        
+        int newTop = *top-1;
+        *top = newTop;
+    } else if(strcmp("NOT", instruction) == 0) {
+        if(n1)
+            stack[*top] = 0;
+        else
+            stack[*top] = 1;
+    } else if(strcmp("AND", instruction) == 0) {
+        if(n2 && n1)
+            stack[*top-1] = 1;
+        else
+            stack[*top-1] = 0;
+        
+        int newTop = *top-1;
+        *top = newTop;
+    } else if(strcmp("OR", instruction) == 0) {
+        if(n2 || n1)
+            stack[*top-1] = 1;
+        else
+            stack[*top-1] = 0;
+        
+        int newTop = *top-1;
+        *top = newTop;
+    } else if(strcmp("0", instruction) == 0) {
+        stack[*top+1] = 0;
+        int newTop = *top+1;
+        *top = newTop;
+    } else if(strcmp("DUP", instruction) == 0) {
+        stack[*top+1] = n1;
+        int newTop = *top+1;
+        *top = newTop;
+    } else if(strcmp("POP", instruction) == 0) {
+        int newTop = *top-1;
+        *top = newTop;
+    } else if(strstr(instruction, "UJP-") != NULL){
+        char jump[2];
+        strncpy(jump, instruction+4, 2);
+        if(strcmp("00", jump) == 0){
+            printf("Operador inválido");
+            return;
+        }
+        int jumpNumber = atoi(jump);
+        if (jumpNumber) {
+            int newPos = *pos-(jumpNumber-1);
+            *pos = newPos;
+        }
+    } else if(strstr(instruction, "UJP+") != NULL){
+        char jump[2];
+        strncpy(jump, instruction+4, 2);
+        if(strcmp("00", jump) == 0){
+            printf("Operador inválido");
+            return;
+        }
+        int jumpNumber = atoi(jump);
+        if (jumpNumber) {
+            int newPos = *pos+(jumpNumber-1);
+            *pos = newPos;
+        }
+    } else if(strstr(instruction, "CJP-") != NULL){
+        if (n1){
+            char jump[2];
+            strncpy(jump, instruction+4, 2);
+            if(strcmp("00", jump) == 0){
+                printf("Operador inválido");
+                return;
+            }
+            int jumpNumber = atoi(jump);
+            if (jumpNumber) {
+                int newPos = *pos-(jumpNumber-1);
+                *pos = newPos;
+            }
+        }
+    } else if(strstr(instruction, "CJP+") != NULL){
+        if (n1){
+            char jump[2];
+            strncpy(jump, instruction+4, 2);
+            if(strcmp("00", jump) == 0){
+                printf("Operador inválido");
+                return;
+            }
+            int jumpNumber = atoi(jump);
+            if (jumpNumber) {
+                int newPos = *pos+(jumpNumber-1);
+                *pos = newPos;
+            }
+        }
+    }
+    else {
         int number = atoi(instruction);
         if (number) {
             stack[*top+1] = number;
             int newTop = *top+1;
             *top = newTop;
+        }
+        else {
+            printf("Operador inválido");
+            return;
         }
     }
     int newPos = *pos+1;
@@ -72,8 +209,8 @@ int main() {
         count++;
         first = 0;
         last = count - 1;
-
-        for (int j = 0; j < count; j++)  {
+        
+        for (int j = 0; j < *top; j++)  {
             printf("%d ", stack[j]);
         }
 
@@ -106,3 +243,4 @@ int main() {
     printf("%d\n", result);
 }
 */
+
